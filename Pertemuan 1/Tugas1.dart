c0339378e50709f88cb8098fdb3f926001a65687
FawwazAfkar <fawwaz.muzakky@mhs.unsoed.ac.id>
2024-09-10 10:22:26 +0700

    initial commit

---
+++ Pertemuan 1/Tugas1.dart
@@ -0,0 +1,13 @@
+// import 'dart:math';
+
+// void main() {
+//   List<String> adjectives = ['Happy', 'Sad', 'Brave', 'Clever', 'Funny'];
+//   List<String> nouns = ['Cat', 'Dog', 'Bird', 'Tree', 'Car'];
+
+//   String adjective = adjectives[Random().nextInt(adjectives.length)];
+//   String noun = nouns[Random().nextInt(nouns.length)];
+
+//   String generatedName = adjective + noun;
+
+//   print('Generated Name: $generatedName');
+// }
