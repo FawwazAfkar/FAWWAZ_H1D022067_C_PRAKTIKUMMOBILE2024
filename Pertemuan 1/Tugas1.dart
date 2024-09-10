2d2f8323bc23de5e37847ef7a0eb7820e29ed3fd
FawwazAfkar <fawwaz.muzakky@mhs.unsoed.ac.id>
2024-09-10 23:03:08 +0700

    tugas 1 upload

--- Pertemuan 1/Tugas1.dart
+++ Pertemuan 1/Tugas1.dart
@@ -1,13 +1,86 @@
-// import 'dart:math';
+//100% halal name generator
 
-// void main() {
-//   List<String> adjectives = ['Happy', 'Sad', 'Brave', 'Clever', 'Funny'];
-//   List<String> nouns = ['Cat', 'Dog', 'Bird', 'Tree', 'Car'];
+//import library
+import 'dart:math';
+import 'dart:io';
 
-//   String adjective = adjectives[Random().nextInt(adjectives.length)];
-//   String noun = nouns[Random().nextInt(nouns.length)];
+//main
+void main() {
+  //array of name
+  List<String> boyFirstNames = [
+    'Muhammad', 
+    'Khalid', 
+    'Ali', 
+    'Zayn', 
+    'Yusuf',
+    'Rayyan',
+    'Hafiz',
+    'Taqy',
+    'Fadhil',
+    'Rashid'
+  ];
+  List<String> girlFirstNames = [
+    'Aisha',
+    'Fatima',
+    'Salma',
+    'Aqeela',
+    'Adzana',
+    'Shafira',
+    'Aaliyah',
+    'Annisa',
+    'Nadya',
+    'Medina'
+  ];
+  List<String> boylastNames = [
+    'Hakim',
+    'Husein',
+    'Malik',
+    'Rahman',
+    'Zakky'
+  ];
+  List<String> girllastNames = [
+    'Salsabila',
+    'Azkiya',
+    'Adhalia',
+    'Shaliha',
+    'Azzahra'
+  ];
 
-//   String generatedName = adjective + noun;
+  //var name
+  String firstName;
+  String lastName;
 
-//   print('Generated Name: $generatedName');
-// }
+  // input gender
+  print('Enter gender (boy/girl):');
+  String? gender = stdin.readLineSync();
+
+  // validate input
+  bool isBoy = true;
+
+  if (gender == null) {
+    print('Invalid gender input. Defaulting to boy.');
+    isBoy = true;
+  } else if (gender.toLowerCase() != 'boy' && gender.toLowerCase() != 'girl') {
+    print('Invalid gender input. Defaulting to boy.');
+    isBoy = true;
+  } else {
+    if (gender.toLowerCase() == 'boy') {
+    isBoy = true;
+    } else if (gender.toLowerCase() == 'girl') {
+      isBoy = false;
+    }
+  } 
+
+  //generate random name based on gender selected
+  if (isBoy) {
+    firstName = boyFirstNames[Random().nextInt(boyFirstNames.length)];
+    lastName = boylastNames[Random().nextInt(boylastNames.length)];
+  } else {
+    firstName = girlFirstNames[Random().nextInt(girlFirstNames.length)];
+    lastName = girllastNames[Random().nextInt(girllastNames.length)];
+  }
+  
+  String fullName = '$firstName $lastName';
+
+  print('Generated Full Name: $fullName');
+}
